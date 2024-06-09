5495fbb93e32f85bb5106eae9691338b0f5b8930
Alireza Fereydoni <Aliirzw@gmail.com>
2024-06-09 23:22:33 +0330

    feat: register models and create migrations

--- blog/models.py
+++ blog/models.py
@@ -7,14 +7,15 @@ class Tag(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    email = models.EmailField()
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    excerpt = models.ManyToManyField(Tag)
-    date = models.DateField(auto_created=True)
-    slug = models.SlugField()
+    excerpt = models.CharField(max_length=150)
+    date = models.DateField(auto_created=True, auto_now=True)
+    slug = models.SlugField(unique=True)
+    image= models.CharField(max_length=100)
     content = models.TextField(max_length=1500)
     
-    author = models.ForeignKey(Author,on_delete=models.CASCADE)
-      
-      
+    author = models.ForeignKey(Author,on_delete=models.SET_NULL, null=True, related_name='post_author')
+    tags = models.ManyToManyField(Tag)
